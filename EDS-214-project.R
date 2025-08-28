1ea447ceb0c7be566a6fa17a9f441194aa1e0671
sandovaljoaquin <joaquinsandoval74@yahoo.com>
2025-08-28 14:41:42 -0700

    Prior to merge

--- EDS-214-project.R
+++ EDS-214-project.R
@@ -12,7 +12,6 @@
 library(here)
 library(tidyverse)
 library(janitor)
-library(lubridate)
 library(patchwork)
 library(gridExtra)
 
@@ -36,7 +35,7 @@ PRM <- read_csv(here("data", "RioMameyesPuenteRoto.csv")) |>
 #Merge all datasets together with rbind to create a 'long' dataframe with variables stacked  
 
 
-Q1_Q2_Q3_PRM <- rbind(Q1, Q2, Q3, PRM) |> 
+Q1_Q2_Q3_PRM <- rbind(Q1, Q2, Q3, PRM) 
 
 
 #Selecting for variables that are relevant to the figure and filtering for dates between 1988-1995
@@ -44,57 +43,58 @@ Q1_Q2_Q3_PRM <- rbind(Q1, Q2, Q3, PRM) |>
 cleaned <- Q1_Q2_Q3_PRM |> 
   select(sample_id, sample_date, nh4_n, ca, mg, no3_n, k) |> 
   filter(sample_date > "1988-01-01" & sample_date < "1995-01-01") |> 
+  arrange(sample_date)
 
-# Using moving_average function along with s apply to create new columns 
+# Using moving_average function along with sapply to create new columns 
 
-cleaned$k_moving <- sapply(
+cleaned$k <- sapply(
   cleaned$sample_date,
   moving_average,
   dates = cleaned$sample_date,
   conc = cleaned$k,
   win_size_wks = 9)
 
-cleaned$no3_n_moving <- sapply(
+cleaned$no3_n <- sapply(
   cleaned$sample_date,
   moving_average,
   dates = cleaned$sample_date,
   conc = cleaned$no3_n,
   win_size_wks = 9)
 
-cleaned$mg_moving <- sapply(
+cleaned$mg <- sapply(
   cleaned$sample_date,
   moving_average,
   dates = cleaned$sample_date,
   conc = cleaned$mg,
   win_size_wks = 9)
 
-cleaned$ca_moving <- sapply(
+cleaned$ca <- sapply(
   cleaned$sample_date,
   moving_average,
   dates = cleaned$sample_date,
   conc = cleaned$ca,
   win_size_wks = 9)
 
-cleaned$nh4_n_moving <- sapply(
+cleaned$nh4_n <- sapply(
   cleaned$sample_date,
   moving_average,
   dates = cleaned$sample_date,
   conc = cleaned$nh4_n,
   win_size_wks = 9)
 
-# Creating dataframe with respect to sample_id, selecting dates from 1988 to 1995, and adding a '9-week' moving average average column for each chemical concentration that we're interested in. 
+# Adding a '9-week' moving average average column for each chemical concentration that we're interested in. 
 
 
 # Plotting moving average of K concentrations vs year
 
 
-k_plot <- ggplot(data = cleaned, 
-                 aes(x = sample_date, 
-                     y = k_moving)) + 
-  geom_line(aes(linetype = sample_id)) + 
-  geom_vline(xintercept = as.Date("1989-09-10"), linetype = "solid") + 
-  xlab(" ") + 
-  ylab("K") + 
+k_plot <- ggplot(data = cleaned,
+                 aes(x = sample_date,
+                     y = k)) +
+  geom_line(aes(linetype = sample_id)) +
+  geom_vline(xintercept = as.Date("1989-09-10"), linetype = "solid") +
+  xlab(" ") +
+  ylab("K") +
   theme(axis.line = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank())
@@ -106,7 +106,7 @@ k_plot
 
 no3_plot <- ggplot(data = cleaned, 
                    aes(x = sample_date, 
-                       y = no3_n_moving)) + 
+                       y = no3_n)) + 
   geom_line(aes(linetype = sample_id))+ 
   geom_vline(xintercept = as.Date("1989-09-10"), linetype = "solid") + 
   xlab(" ") + 
@@ -122,7 +122,7 @@ no3_plot
 
 mg_plot <- ggplot(data = cleaned, 
                   aes(x = sample_date, 
-                      y = mg_moving)) + 
+                      y = mg)) + 
   geom_line(aes(linetype = sample_id)) + 
   geom_vline(xintercept = as.Date("1989-09-10"), linetype = "solid") + 
   xlab(" ") + 
@@ -138,8 +138,8 @@ mg_plot
 
 
 ca_plot <- ggplot(data = cleaned, 
-                  aes(x = sample_date, 
-                      y = ca_moving)) + 
+                  aes(x = sample_id, 
+                      y = ca)) + 
   geom_line(aes(linetype = sample_id)) + 
   geom_vline(xintercept = as.Date("1989-09-10"), linetype = "solid") + 
   xlab(" ") + 
@@ -157,7 +157,7 @@ ca_plot
 
 nh4_plot <- ggplot(data = cleaned, 
                    aes(x = sample_date, 
-                       y = nh4_n_moving)) + 
+                       y = nh4_n)) + 
   geom_line(aes(linetype = sample_id)) + 
   geom_vline(xintercept = as.Date("1989-09-10"), linetype = "solid") + 
   xlab(" ") + 
