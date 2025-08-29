e15338c0b68a002eb37a41c67c842ea1e6c329cb
sandovaljoaquin <joaquinsandoval74@yahoo.com>
2025-08-29 08:39:50 -0700

    Added roxygen

--- R/moving_average.R
+++ R/moving_average.R
@@ -1,3 +1,15 @@
+#' Moving average function
+#'
+#' @param sample_date : when data was collected. Reference date for 9 week window. 
+#' @param dates : adjacent dates with associated ion concentrations that must be within 4.5 weeks before or after sample_date. 
+#' @param conc : Concentration of ion
+#' @param win_size_wks : 9 week window
+#'
+#' @returns  : 9 week moving average of concentration 
+#' @export
+#'
+#' @examples
+
 moving_average <- function(sample_date, dates, conc, win_size_wks) {
   # Dates in the window
   is_in_window <- (dates > sample_date - (win_size_wks / 2) * 7) &
