bad5ce8e3429e1a7b37767a651e85cb7b0ce6c03
sandovaljoaquin <joaquinsandoval74@yahoo.com>
2025-08-28 20:07:53 -0700

    Updated scripts

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -1,12 +1,27 @@
-#Merge all datasets together with rbind to create a 'long' dataframe with variables stacked  
+# Merge all datasets together with rbind to create a 'long' dataframe with variables stacked.  
 
+q1_q2_q3_prm <- rbind(q1, q2, q3, prm) 
 
-Q1_Q2_Q3_PRM <- rbind(Q1, Q2, Q3, PRM)
+# Selecting for variables that are relevant to the figure and filtering for dates between 1988-1995. Also, pivoting longer to create columns 'stream_ion' and 'concentration'. 
 
+cleaned <- q1_q2_q3_prm |> 
+  select(sample_id, sample_date, nh4_n, ca, mg, no3_n, k) |>
+  filter(sample_date > "1988-01-01" & sample_date < "1995-01-01") |> 
+  arrange(sample_date) |> 
+  pivot_longer(cols = c("nh4_n", "ca", "mg", "no3_n", "k"), names_to = "stream_ion", values_to = "concentration")
 
-#Selecting for variables that are relevant to the figure.
+# Source moving average function from r script.
 
+source(here("R", "moving_average.R"))
 
-cleaned <- Q1_Q2_Q3_PRM |> 
-  select(sample_id, sample_date, nh4_n, ca, mg, no3_n, k) |> 
-  filter(sample_date > "1988-01-01" & sample_date < "1995-01-01")
+# Adding a '9-week' moving average column for each observation under the concentration column. 
+
+rolling_average <- cleaned |> 
+  group_by(sample_id, stream_ion) |> 
+  mutate(moving_avg = sapply(
+    sample_date,
+    moving_average,
+    dates = sample_date,
+    conc = concentration, 
+    win_size_wks = 9)) |> 
+  ungroup()
